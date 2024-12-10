5dea49cfea53753c38cb5850e74fb858b0b6c704
ahibrah-99 <ah.moh.ibrahim@gmail.com>
2024-12-10 16:05:40 +0100

    completed integration tests

--- tests/temp.rs
+++ tests/temp.rs
@@ -1,23 +1,85 @@
 #[cfg(test)]
 mod tests {
+
+    use moseiik::main::*;
+
     #[test]
     #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
     fn test_x86() {
-        // TODO
-        // test avx2 or sse2 if available
-        assert!(false);
+        // Arguments to run program
+        let args = Options {
+            image: String::from("assets/kit.jpeg"),
+            output: String::from("out.png"),
+            tiles: String::from("assets/images/"),
+            scaling: 1,
+            tile_size: 25,
+            remove_used: false,
+            verbose: false,
+            simd: true,
+            num_thread: 1,
+        };
+        
+        // Apply computte mosaic
+        compute_mosaic(args);
+        
+        // Verify wether generated image corresponds to ground-truth or not to pass test
+        let generated = image::open("out.png").unwrap();
+        let ground_truth = image::open("assets/ground-truth-kit.png").unwrap();
+        assert_eq!(generated.height(), ground_truth.height(), "Generated mosaic does not match ground truth for x86");
+
+        //assert!(false);
     }
 
     #[test]
     #[cfg(target_arch = "aarch64")]
     fn test_aarch64() {
-        //TODO
-        assert!(false);
+
+        // Arguments to run program
+        let args = Options {
+            image: String::from("assets/kit.jpeg"),
+            output: String::from("out.png"),
+            tiles: String::from("assets/images/"),
+            scaling: 1,
+            tile_size: 25,
+            remove_used: false,
+            verbose: false,
+            simd: true,
+            num_thread: 1,
+        };
+        
+        // Apply computte mosaic
+        compute_mosaic(args);
+    
+        // Verify wether generated image corresponds to ground-truth or not to pass test
+        let generated = image::open("out.png").unwrap();
+        let ground_truth = image::open("assets/ground-truth-kit.png").unwrap();
+        assert_eq!(generated.height(), ground_truth.height(), "Generated mosaic does not match ground truth for aarch64");
+        
+        //assert!(false);
     }
 
     #[test]
     fn test_generic() {
-        //TODO
-        assert!(false);
+        
+        let args = Options {
+            image: String::from("assets/kit.jpeg"),
+            output: String::from("out.png"),
+            tiles: String::from("assets/images/"),
+            scaling: 1,
+            tile_size: 25,
+            remove_used: false,
+            verbose: false,
+            simd: false,
+            num_thread: 1,
+        };
+    
+        compute_mosaic(args);
+    
+        let generated = image::open("out.png").unwrap();
+        let ground_truth = image::open("assets/ground-truth-kit.png").unwrap();
+    
+        assert_eq!(generated.height(), ground_truth.height(), "Generated mosaic does not match ground truth for generic implementation");
+        
+        //assert!(false);
     }
 }
